63c98795b829561c53735483b2c2fb85020500e7
er02990 <54376608+er02990@users.noreply.github.com>
2020-11-17 19:46:15 -0500

    Create UserThread.java

---
+++ project/UserThread.java
@@ -0,0 +1,20 @@
+package net.codejava.networking.chat.server;
+import java.net.Socket;
+
+public class UserThread {
+
+	public UserThread(Socket sock, Sockets sockets) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void sendMessage(String message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
