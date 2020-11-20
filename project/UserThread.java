6c3c2741feb043e874ebec1cf88749fb580a116a
er02990 <54376608+er02990@users.noreply.github.com>
2020-11-19 19:11:28 -0500

    Update UserThread.java

--- project/UserThread.java
+++ project/UserThread.java
@@ -1,50 +1,75 @@
-
-import java.net.*;
 import java.io.*;
+import java.net.*;
+import java.util.*;
  
-public class ChatClient {
-    private String hostname;
-    private int port;
-    private String userName;
+/**
+ * This thread handles connection for each connected client, so the server
+ * can handle multiple clients at the same time.
+ *
+ * @author www.codejava.net
+ */
+public class UserThread extends Thread {
+    private Socket socket;
+    private ChatServer server;
+    private PrintWriter writer;
  
-    public ChatClient(String hostname, int port) {
-        this.hostname = hostname;
-        this.port = port;
+    public UserThread(Socket socket, ChatServer server) {
+        this.socket = socket;
+        this.server = server;
     }
  
-    public void execute() {
+    public void run() {
         try {
-            Socket socket = new Socket(hostname, port);
+            InputStream input = socket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
  
-            System.out.println("Connected to the chat server");
+            OutputStream output = socket.getOutputStream();
+            writer = new PrintWriter(output, true);
  
-            new ReadThread(socket, this).start();
-            new WriteThread(socket, this).start();
+            printUsers();
  
-        } catch (UnknownHostException ex) {
-            System.out.println("Server not found: " + ex.getMessage());
+            String userName = reader.readLine();
+            server.addUserName(userName);
+ 
+            String serverMessage = "New user connected: " + userName;
+            server.broadcast(serverMessage, this);
+ 
+            String clientMessage;
+ 
+            do {
+                clientMessage = reader.readLine();
+                serverMessage = "[" + userName + "]: " + clientMessage;
+                server.broadcast(serverMessage, this);
+ 
+            } while (!clientMessage.equals("bye"));
+ 
+            server.removeUser(userName, this);
+            socket.close();
+ 
+            serverMessage = userName + " has quitted.";
+            server.broadcast(serverMessage, this);
+ 
         } catch (IOException ex) {
-            System.out.println("I/O Error: " + ex.getMessage());
+            System.out.println("Error in UserThread: " + ex.getMessage());
+            ex.printStackTrace();
         }
- 
     }
  
-    void setUserName(String userName) {
-        this.userName = userName;
+    /**
+     * Sends a list of online users to the newly connected user.
+     */
+    void printUsers() {
+        if (server.hasUsers()) {
+            writer.println("Connected users: " + server.getUserNames());
+        } else {
+            writer.println("No other users connected");
+        }
     }
  
-    String getUserName() {
-        return this.userName;
-    }
- 
- 
-    public static void main(String[] args) {
-        if (args.length < 2) return;
- 
-        String hostname = args[0];
-        int port = Integer.parseInt(args[1]);
- 
-        ChatClient client = new ChatClient(hostname, port);
-        client.execute();
+    /**
+     * Sends a message to the client.
+     */
+    void sendMessage(String message) {
+        writer.println(message);
     }
 }
